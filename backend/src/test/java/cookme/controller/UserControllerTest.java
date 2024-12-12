91f4f7d2efc9655f5a55a94d695786711ada1d15
malekch-15 <malekchenenaoui@gmail.com>
2024-12-13 00:36:42 +0100

    update the UserControllerTest

--- backend/src/test/java/cookme/controller/UserControllerTest.java
+++ backend/src/test/java/cookme/controller/UserControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
