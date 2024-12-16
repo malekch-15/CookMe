8e3eb482355b409794319f88f5a8d86a39ebcd42
malekch-15 <malekchenenaoui@gmail.com>
2024-12-16 10:34:19 +0100

    update userController

--- backend/src/main/java/cookme/security/UserController.java
+++ backend/src/main/java/cookme/security/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 
@@ -18,23 +19,26 @@ import java.util.List;
 @RequestMapping("/api/users/me")
 @RequiredArgsConstructor
 public class UserController {
-public final AppUserService appUserService;
-   @GetMapping()
-   public AppUser getCurrentUserDetails(@AuthenticationPrincipal OAuth2User user) {
-       if(user!=null) { String name = user!= null ? user.getName() : "anonymousUser";
-           String login = user.getAttribute("login");
-           String avatarUrl = user.getAttribute("avatar_url");
-           AppUser currentUser = appUserService.getUserById(name);
-           List<Recipe> favorites = appUserService.getUserFavorites(currentUser.id());
-           List<RecipeIngredient> ingredient = appUserService.getUserIngredient(currentUser.id());
-           System.out.println(user.getAttributes());
-           return new AppUser(
-                   name,
-                   login,
-                   avatarUrl,
-                   ingredient,
-                   favorites
-           );}else throw new UserNotFoundException("User with ID nonexistentUser not found");
+    public final AppUserService appUserService;
 
-   }
+    @GetMapping()
+    public AppUser getCurrentUserDetails(@AuthenticationPrincipal OAuth2User user) {
+        String name = user != null ? user.getName() : "anonymousUser";
+        if (user != null) {
+            String login = user.getAttribute("login");
+            String avatarUrl = user.getAttribute("avatar_url");
+            AppUser currentUser = appUserService.getUserById(name);
+            List<Recipe> favorites = appUserService.getUserFavorites(currentUser.id());
+            List<RecipeIngredient> ingredient = appUserService.getUserIngredient(currentUser.id());
+            System.out.println(user.getAttributes());
+            return new AppUser(
+                    name,
+                    login,
+                    avatarUrl,
+                    ingredient,
+                    favorites
+            );
+        } else throw new UserNotFoundException("User with ID nonexistentUser not found");
+
+    }
 }
--- backend/src/test/java/cookme/controller/UserControllerTest.java
+++ backend/src/test/java/cookme/controller/UserControllerTest.java
@@ -11,13 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
+
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
-import java.util.Map;
+
 
 
 import static org.mockito.Mockito.when;
