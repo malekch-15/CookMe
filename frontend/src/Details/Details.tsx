712fcfca4c920bbe6704d13c0f30d47553310bb4
malekch-15 <malekchenenaoui@gmail.com>
2024-12-17 00:07:05 +0100

    details page css updated

--- frontend/src/Details/Details.tsx
+++ frontend/src/Details/Details.tsx
@@ -1,60 +1,67 @@
-import {Recipe} from "../Model/Recipe.ts";
-import {useNavigate} from "react-router-dom";
-import "../css/Details.css"
+import { Recipe } from "../Model/Recipe.ts";
+import { useNavigate } from "react-router-dom";
+import "../css/Details.css";
 
 type DetailsProps = {
-    recipe: Recipe
-}
+    recipe: Recipe;
+};
 
-export default function Details({recipe}: Readonly<DetailsProps>) {
-    console.log(recipe)
-    const navigate = useNavigate()
+export default function Details({ recipe }: Readonly<DetailsProps>) {
+    console.log(recipe);
+    const navigate = useNavigate();
 
     const handleEdit = (id: string) => {
         navigate(`/details/${id}/edit`);
-    }
-    const preparationSteps = recipe.preparation.split(/[.]\s/);
-    return (
-        <div className="details">
-            {recipe.imageUrl ? (
-                <img
-                    src={recipe.imageUrl}
-                    alt={recipe.name}
-                    className="details-image"
-                />
-            ) : (
-                <div className="no-image">Image not available</div>
-            )}
+    };
 
-            <div className="details-text">
-                <h1 className="details-title">{recipe.name}</h1>
-                <p className="details-description">{recipe.description}</p>
+    const preparationSteps = recipe.preparation.split(/[.]\s/);
+
+    return (
+        <>
+            <div className="details-header">
+                    <h1 className="details-title">{recipe.name}</h1>
+                    <p className="details-description">{recipe.description}</p>
+            </div>
+
+            <div className="details-content">
+                {recipe.imageUrl ? (
+                    <img
+                        src={recipe.imageUrl}
+                        alt={recipe.name}
+                        className="details-image"
+                    />
+                ) : (
+                    <div className="no-image">Image not available</div>
+                )}
                 <div className="details-ingredients">
                     <h3>Ingredients:</h3>
-                    <ul>{recipe.ingredients.map((ingredient) => (
-                        <li key={ingredient.ingredient.id}>
-                            {ingredient.quantity} {ingredient.ingredient.name}
-                        </li>
-                    ))}
-
+                    <ul>
+                        {recipe.ingredients.map((ingredient) => (
+                            <li key={ingredient.ingredient.id}>
+                                {ingredient.quantity} {ingredient.ingredient.name}
+                            </li>
+                        ))}
                     </ul>
-
-                    <div className="details-preparation">
-                        <h3>Preparation:</h3>
-                        <ol>
-                            {preparationSteps.map((step, index) => (
-                                <li key={index}>{step}</li>
-                            ))}
-                        </ol>
-
-
                 </div>
 
-                <button className="show-more-button" onClick={() => handleEdit(recipe?.id)}>Edit
+
+            </div>
+            <div className="details-preparation">
+                <h3>Preparation:</h3>
+                <ol>
+                    {preparationSteps.map((step, index) => (
+                        <li key={index}>{step}</li>
+                    ))}
+                </ol>
+            </div>
+            <div className="details-footer">
+                <button
+                    className="show-more-button"
+                    onClick={() => handleEdit(recipe?.id)}
+                >
+                    Edit
                 </button>
             </div>
-        </div>
-</div>
-)
-    ;
+        </>
+    );
 }
--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -27,14 +27,12 @@ export default function Home(props:Readonly<HomeProps>){
     return(
         <>
 
-            <div>
-                <Searchbar onSearch={handleSearch}/>
-
+            <div className="home-title">
                 <h1>Recipes</h1>
-                <p> CookMe is your source for thousands of delicious, easy and quick recipes, healthy meals,
+                <p className="home-description"> CookMe is your source for thousands of delicious,<br/> easy and quick recipes, healthy meals,
                     dinner ideas, recipes for kids, holiday menus, and more</p>
             </div>
-
+            <Searchbar onSearch={handleSearch}/>
             <div className="card-gallery">
 
                 {filteredRecipes.length === 0 ? (
