82064e3297d3c76191b11316b9bd3a1d2126e03f
malekch-15 <malekchenenaoui@gmail.com>
2024-12-17 20:18:03 +0100

    create ingredient  unittest

--- backend/src/test/java/cookme/controller/IngredientControllerTest.java
+++ backend/src/test/java/cookme/controller/IngredientControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -33,7 +32,8 @@ class IngredientControllerIntegrationTest {
     @BeforeEach
     void setUp() {
         BaseIngredient ingredient1;
-        BaseIngredient ingredient2;  ingredient1 = new BaseIngredient("1", "Tomato");
+        BaseIngredient ingredient2;
+        ingredient1 = new BaseIngredient("1", "Tomato");
         ingredient2 = new BaseIngredient("2", "Salt");
        ingredientsRepo.save(ingredient1);
       ingredientsRepo.save(ingredient2);
