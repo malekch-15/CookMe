f5f4d932e46acef66365e2da952baaa5e2c20f6d
malekch-15 <malekchenenaoui@gmail.com>
2024-12-19 02:39:04 +0100

    sonar updated

--- backend/src/main/java/cookme/services/AppUserService.java
+++ backend/src/main/java/cookme/services/AppUserService.java
@@ -13,8 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
+
 
 
 @Service
